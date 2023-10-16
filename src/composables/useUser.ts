81d3e31cbd1725bbfc9fe11b3a76e8e2fec99a5f
Алексей Кривобородов <@itstep.loc>
2023-10-16 19:27:03 +0600

    user update

--- src/composables/useUser.ts
+++ src/composables/useUser.ts
@@ -1,4 +1,4 @@
-import { collection, getDocs, addDoc } from 'firebase/firestore'
+import { collection, getDocs, addDoc, type DocumentData } from 'firebase/firestore'
 import { db, storage } from '@/firebase'
 import { getStorage, uploadBytes, getDownloadURL } from 'firebase/storage'
 import { ref, computed } from 'vue'
@@ -6,9 +6,11 @@ import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 
 export const useUser = () => {
   const user = ref()
+  const userList = ref([] as DocumentData)
 
   const loading = ref({
-    user: false
+    user: false,
+    userList: false
   })
 
   const auth = getAuth()
@@ -42,12 +44,34 @@ export const useUser = () => {
     loading.value.user = true
     try {
       if (userRemake.value) {
-        await addDoc(collection(db, 'users'), userRemake.value)
+        await getAllUsers()
+        if (!checkUserInDatabase()) {
+          await addDoc(collection(db, 'users'), userRemake.value)
+        } else {
+          console.error('User already in database')
+        }
       }
       loading.value.user = false
     } catch (error) {
       console.error(error)
     }
+  }
+
+  async function getAllUsers() {
+    loading.value.userList = true
+    try {
+      const querySnapshot = await getDocs(collection(db, 'users'))
+      querySnapshot.forEach((doc) => {
+        userList.value.push(doc.data())
+      })
+      loading.value.userList = false
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  function checkUserInDatabase() {
+    return userList.value.some((item: any) => item.uid === userRemake.value?.uid)
   }
 
   function googleLogout() {
